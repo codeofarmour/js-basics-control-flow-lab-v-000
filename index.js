573b3401f6ddfd34aa4e758c69d71f6fa1cb411b
Terri Riggle <codeofarmour@outlook.com>
2018-02-03 07:52:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,8 +15,11 @@ function switchOnCharmFromTip(tip) {
     case 'generous':
       return 'Thank you so much.';
       break;
-
+    case 'not as generous':
+      return 'Thank you.';
+      break;
     default:
-
+      return 'Bye.';
+      break;
   }
 }
