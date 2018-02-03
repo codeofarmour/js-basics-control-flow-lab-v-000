fcd5ba5d628a8591a0801a8e7eef0652de882ee0
Terri Riggle <codeofarmour@outlook.com>
2018-02-03 07:27:04 +0000

    automatically backed up by learn

--- index.js
+++
@@ -1,9 +0,0 @@
-const scuberGreetingForFeet() = 'This one is on me!';
-
-const switchOnCharmFromTip = 'generous';
-let response;
-switch(switchOnCharmFromTip) {
-  case 'generous':
-  response = 'Thank you so much.';
-
-}
